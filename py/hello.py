93414e6ce943e35f9530a778cb4e4a4103fb6217
ap <mail.arorapk@gmail.com>
2022-04-19 22:04:53 -0400

    now

--- py/hello.py
+++ py/hello.py
@@ -1,3 +1,3 @@
 print(
-    "Hello world!, sit tight; this is going to be a heel of roler coster ride of life"
+    "Hello world!, sit tight; this is going to be a hell of roler coster ride of my life."
 )
