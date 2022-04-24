45a80b4498ce66b3192a94735aabe8ac8b2cb13b
ap <mail.arorapk@gmail.com>
2022-04-23 21:38:14 -0400

    commit

---
+++ py/calc.py
@@ -0,0 +1,6 @@
+# Sum
+print("sum:", 4**2)
+# Greater
+print(3 + 9.1 == 5 + 7.1)
+# Is it greater?
+print("Is it gtreater?", 5 < -2)
--- py/hello.py
+++ py/hello.py
@@ -1,4 +1,5 @@
 print("Hello 1")
 print("Hello 2")
 print("Hello 4")
+
 print("Hello 5")
