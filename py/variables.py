76eac699600ab833f718d96485d49934377963c8
ap <mail.arorapk@gmail.com>
2022-04-24 14:28:04 -0400

    strings

--- py/variables.py
+++ py/variables.py
@@ -1,8 +1,8 @@
 # about variables
 my_age = 2022 - 1985
 my_height = "5'10''"
-print("I am ", my_age, " years old!")
-print("I am ", my_height, "tall")
+# print("I am ", my_age, " years old!")
+# print("I am ", my_height, "tall")
 
 
 cars = 100
@@ -13,10 +13,27 @@ cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passanger_per_car = passangers / cars_driven
-
+"""""
 print("There are", cars, "cars avaiable!")
 print("There are only", drivers, "drivers avaiable!")
 print("One car has", space_in_a_car, "spaces!")
 print("There are", passangers, "passangers today.")
 print("We can transport ", carpool_capacity, "people today.")
 print("We need to put about", average_passanger_per_car, "per car.")
+"""
+
+# more variablesx
+
+my_name = "Puneet Arora"
+my_age = 2022 - 1985
+my_height = 5.11 * 12
+my_weight = 210 / 400
+my_eyes = "Blk"
+
+total = my_height + my_weight + my_age
+
+print(f"Let's talk about {my_name}.")
+print(f"He is {my_height} inch tall.")
+print(f"His eyes are {my_eyes}.")
+
+print(f"If I add {my_age},{round(my_height)} and {my_weight}.I get {total}.")
