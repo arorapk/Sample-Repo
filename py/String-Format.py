2abea1cde1e16d3e22b76e96dcb0bdf79e425670
ap <mail.arorapk@gmail.com>
2022-04-24 16:56:08 -0400

    string.format

--- py/String-Format.py
+++ py/String-Format.py
@@ -1,16 +1,26 @@
 types_of_people = 10
-x = f"there are {types_of_people} types of people."
+
+# Using integer variable "types_of_people" in a fstring
+x = f"There are {types_of_people} types of people."
 binary = "binary"
 do_not = "don't"
+
+# Using strings in fstring
 y = f"Those who know {binary} and those who {do_not}."
 
+# Printing string validables
 print(x)
 print(y)
 
+# Printing string validables
 print(f"I said: {x}")
 print(f"I also said :'{y}'")
 
-hilarious = True
-joke_evaludation = "Isn't that joke funny?!{}"
-print(joke_evaludation.format(hilarious))
-# print(joke_evaludation)
+# using string.format () - one of many ways
+
+hilarious = Really?
+joke_evaluation = "Isn't that joke funny?!{}"
+print(joke_evaluation.format(hilarious))
+
+# using string.format () - another way of achieving the same output in one line
+print("Isn't that joke funny?!{}").format("Really")
